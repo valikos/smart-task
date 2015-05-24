42d0399917a537b51fc023f2866e7748f4385b4a
valikos <valikos.ost@gmail.com>
2015-05-24 21:12:01 +0300

    Feauture tests for project

--- spec/support/pages/projects_page.rb
+++ spec/support/pages/projects_page.rb
@@ -1,8 +1,11 @@
 class ProjectsPage < SitePrism::Page
   set_url '/'
 
-  element :new_project,      '.project-add'
-  element :new_project_name, '.new-project-name'
-  element :add_new_project,  '.add-new-project'
-
+  element :new_project,       '.project-add'
+  element :new_project_name,  '.new-project-name'
+  element :add_new_project,   '.add-new-project'
+  element :edit_project,      '.edit-project'
+  element :edit_name_field,   '.edit-name'
+  element :confirm_name_edit, '.apply-changes'
+  element :remove_project,    '.remove-project'
 end
